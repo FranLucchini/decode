54a75d97ba1df5ca881430943a9dd6a8d8136c36
franlucchini <flucchini@uc.cl>
2023-08-23 16:00:25 -0400

    run model in notebook

--- FracTAL_ResUNet/nn/loss/mtsk_loss.py
+++ FracTAL_ResUNet/nn/loss/mtsk_loss.py
@@ -1,4 +1,5 @@
 from FracTAL_ResUNet.nn.loss.ftnmt_loss import *
+from mxnet import cpu
 
 class mtsk_loss(object):
     """
@@ -13,20 +14,31 @@ class mtsk_loss(object):
         self.skip = NClasses
 
     def loss(self,_prediction,_label):
-
-        pred_segm  = _prediction[0]
-        pred_bound = _prediction[1]
-        pred_dists = _prediction[2]
+        # z = z.as_in_context(gpu(0))
+        pred_segm  = _prediction[0].as_in_context(cpu(0))
+        pred_bound = _prediction[1].as_in_context(cpu(0))
+        pred_dists = _prediction[2].as_in_context(cpu(0))
         
         # In our implementation of the labels, we stack together the [segmentation, boundary, distance] labels, 
-        # along the channel axis. 
-        label_segm  = _label[:,:self.skip,:,:]
-        label_bound = _label[:,self.skip:2*self.skip,:,:]
-        label_dists = _label[:,2*self.skip:,:,:]
+        # along the channel axis.
+        # print(type(_label), len(_label))
+        label_segm  = _label[0]#.as_in_context(cpu(0))
+        label_bound = _label[1]#.as_in_context(cpu(0))
+        label_dists = _label[2]#.as_in_context(cpu(0))
+        # label_segm  = _label[:,:self.skip,:,:]
+        # label_bound = _label[:,self.skip:2*self.skip,:,:]
+        # label_dists = _label[:,2*self.skip:,:,:]
 
+        # print(pred_segm.ctx, label_segm.ctx)
+        # print(pred_segm.shape, label_segm.shape)
+        loss_segm  = self.ftnmt(pred_segm,  label_segm)
 
-        loss_segm  = self.ftnmt(pred_segm,   label_segm)
+        # print(pred_bound.ctx, label_bound.ctx)
+        # print(pred_bound.shape, label_bound.shape)
         loss_bound = self.ftnmt(pred_bound, label_bound)
+
+        # print(pred_dists.ctx, label_dists.ctx)
+        # print(pred_dists.shape, label_dists.shape)
         loss_dists = self.ftnmt(pred_dists, label_dists)
 
         return (loss_segm+loss_bound+loss_dists)/3.0
